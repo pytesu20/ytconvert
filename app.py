24fd2e4cf52563652560b4dc6cf9c9fd00eb1bdb
pytesu20 <60126993+pytesu20@users.noreply.github.com>
2020-01-21 13:07:15 +0700

    Add files via upload

---
+++ app.py
@@ -0,0 +1,19 @@
+
+
+
+import webbrowser
+
+def openlink(txt1):
+    a_website = txt1
+    webbrowser.get('firefox').open_new_tab(a_website)
+
+def convert(txt):
+    ytid = txt.replace('https://www.youtube.com/watch?v=', "")
+    yturl = 'https://img.youtube.com/vi/' + ytid + '/maxresdefault.jpg'
+    return yturl
+
+
+url = input('enter links: ')
+final_url = convert(url)
+openlink(final_url)
+
